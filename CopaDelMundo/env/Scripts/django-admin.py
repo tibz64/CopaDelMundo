ab34a9420d0c2878d1046a13909e361fa6062a48
Tibz64 <thibaud.duplantier@gmail.com>
2018-05-21 21:37:29 +0200

    Ajoutez des fichiers projet.

---
+++ CopaDelMundo/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Thibaud\Desktop\Django\CopaDelMundo\CopaDelMundo\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
