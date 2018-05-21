f66938dd99b8669e9b5cdd9e5e4cb2295bee8e8c
Tibz64 <thibaud.duplantier@gmail.com>
2018-05-21 21:52:38 +0200

    Initialisation Projet

--- CopaDelMundo/CopaDelMundo/settings.py
+++ CopaDelMundo/CopaDelMundo/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'CopaDelMundo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'CopaDelMundo',
+        'USER': 'postgres',
+        'PASSWORD': '020487',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
