ea0fb9c4e709a55ac5fba9620a52f59cc09dc49b
Tibz64 <thibaud.duplantier@gmail.com>
2018-05-27 23:33:04 +0200

    Modelisation

--- CopaDelMundo/app/models.py
+++ CopaDelMundo/app/models.py
@@ -9,28 +9,29 @@ from django.contrib.auth.models import User
 
 class Team(models.Model):
     name=models.CharField(max_length=200,unique=True)
-    user_admin=models.ForeignKey(User,on_delete=models.CASCADE)
+    user_admin=models.ForeignKey(User,on_delete=models.CASCADE, null=True)
 
 class UserSettings(models.Model):
     user=models.ForeignKey(User, on_delete=models.CASCADE)
-    team=models.ForeignKey(Team, on_delete=models.SET_NULL)
-    points=models.IntegerField()
+    team=models.ForeignKey(Team, on_delete=models.SET_NULL,null=True)
+    points=models.PositiveIntegerField(default=0)
 
 class FootballTeam(models.Model):
     name=models.CharField(max_length=200,unique=True)
 
+
 class Match(models.Model):
-    home_team=models.ForeignKey(FootballTeam,related_name='related_primary_footballteam',on_delete=models.CASCADE)
-    away_team=models.ForeignKey(FootballTeam,related_name='related_secondary_footballteam',on_delete=models.CASCADE)
-    home_goals=models.IntegerField()
-    away_goals=models.IntegerField()
-    home_odds=models.DecimalField()
-    away_odds=models.DecimalField()
-    home_points=models.IntegerField()
-    away_points=models.IntegerField()
-    date=models.DateTimeField()
+    home_team=models.ForeignKey(FootballTeam,related_name='home',on_delete=models.CASCADE)
+    away_team=models.ForeignKey(FootballTeam,related_name='away',on_delete=models.CASCADE)
+    home_goals=models.PositiveIntegerField(null=True)
+    away_goals=models.PositiveIntegerField(null=True)
+    home_odds=models.DecimalField(max_digits=5,decimal_places=2,null=True)
+    away_odds=models.DecimalField(max_digits=5,decimal_places=2,null=True)
+    home_points=models.PositiveIntegerField(null=True)
+    away_points=models.PositiveIntegerField(null=True)
+    date=models.DateTimeField(null=True)
 
 class Pick(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     match=models.ForeignKey(Match,on_delete=models.CASCADE)
-    home_picked=models.BooleanField()
+    home_picked=models.NullBooleanField()
